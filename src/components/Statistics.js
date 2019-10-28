cd667df5bc605b549f1e6b3fd1179fe14750a488
Chisw <chisw@qq.com>
2019-10-28 22:19:35 +0800

    post 2019/10/28

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -39,6 +39,7 @@ const data = [
   { date: '2019/10/23', count: 862 },
   { date: '2019/10/25', count: 272 },
   { date: '2019/10/27', count: 941 },
+  { date: '2019/10/28', count: 291 },
 ]
 
 const cols = {
