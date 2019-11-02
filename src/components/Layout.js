6e25a83ee0772583233c3232e552d3ed81bf8f6c
Chisw <chisw@qq.com>
2019-11-02 14:50:07 +0800

    change logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.sass'
 import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from "gatsby"
+import { withPrefix } from 'gatsby'
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
@@ -14,29 +14,11 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon-.png`}
-        />
         <link
           rel="icon"
           type="image/png"
           href={`${withPrefix("/")}img/favicon-32x32-.png`}
           sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16-.png`}
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
-          color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
 
