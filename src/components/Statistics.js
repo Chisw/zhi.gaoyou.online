4961f7e2026d8c170fb1218d4a0036835853da23
Chisw <chisw@qq.com>
2019-11-11 21:53:12 +0800

    post 2019/11/11

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -48,6 +48,7 @@ const data = [
   { date: '2019/11/05', count: 662 },
   { date: '2019/11/06', count: 382 },
   { date: '2019/11/10', count: 560 },
+  { date: '2019/11/11', count: 539 },
 ]
 
 const cols = {
