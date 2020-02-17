420fc17dd8ef69382fad0f5f6f4bf4faa3e25b85
Chisw <chisw@qq.com>
2020-02-17 22:22:28 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Center from '../components/Center'
 
 const NotFoundPage = () => (
   <Layout>
-    <div style={{
-      padding: '180px',
-      textAlign: 'center',
-      background: '#fcfcfc'
-    }}>
-      <h1 style={{fontSize: '60px'}}>404 NOT FOUND <span role="img" aria-label="ghost">👻</span></h1>
-      <p>访问路径出错，或者该页面正在建设中..</p>
-      <p>The access path is wrong, or the page is under construction..</p>
+    <div className="pt-20 pb-8">
+      <Center mobilePadding>
+        <h1 style={{fontSize: '60px'}}>404 NOT FOUND <span role="img" aria-label="ghost">👻</span></h1>
+        <p>访问路径出错，或者该页面正在建设中..</p>
+        <p>The access path is wrong, or the page is under construction..</p>
+      </Center>
     </div>
   </Layout>
 )
