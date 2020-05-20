0d25323b8e74f588c920c45974727d4a52191c6d
Chisw <chisw@qq.com>
2020-05-20 09:59:24 +0800

    improve 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,13 @@ import Center from '../components/Center'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="pt-20 pb-8">
-      <Center>
-        <h1 style={{fontSize: '60px'}}>404 NOT FOUND <span role="img" aria-label="ghost">👻</span></h1>
-        <p>访问路径出错，或者该页面正在建设中..</p>
-        <p>The access path is wrong, or the page is under construction..</p>
-      </Center>
-    </div>
+    <Center className="py-20">
+      <div className="pt-40 pb-48 text-center">
+        <h1 className="mb-4 text-5xl text-gray-700 font-din">404 NOT FOUND <span role="img" aria-label="ghost">👻</span></h1>
+        <p className="text-gray-500">访问路径出错，或者该页面正在建设</p>
+        <p className="text-gray-500">The access path is wrong, or the page is under construction</p>
+      </div>
+    </Center>
   </Layout>
 )
 
