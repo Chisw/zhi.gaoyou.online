2273db9eb14620a6060002201bf87cd3c2dd2d28
Chisw <chisw@qq.com>
2020-06-21 11:28:59 +0800

    post 2020/06/21

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -94,6 +94,7 @@ const counts = [
   { date: '2020/06/15', count: 334 },
   { date: '2020/06/16', count: 285 },
   { date: '2020/06/18', count: 386 },
+  { date: '2020/06/21', count: 816 },
 ]
 
 const allCounts = Interval
