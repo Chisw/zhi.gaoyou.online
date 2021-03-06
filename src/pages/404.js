38856be77c353bddfb25a5059dfa9761cfaf3931
Chisw <chisw@qq.com>
2021-03-06 22:27:41 +0800

    use React 17 and Gatsby starter

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import cloud from '../img/cloud.png'
 const NotFoundPage = () => (
   <Layout>
     <Center className="py-20">
-      <div className="pt-40 pb-48 text-center bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${cloud})`}}>
+      <div className="pt-40 pb-48 text-center bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${cloud})` }}>
         <h1 className="mb-12 text-6xl text-gray-700 font-kxzd">
           <span className="inline-block -mx-1 w-24 h-24 border-4 border-double bg-white">肆</span>
           <span className="inline-block -mx-1 w-24 h-24 border-4 border-double bg-white rounded-full">零</span>
